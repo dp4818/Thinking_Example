a2acb22799326b899897bebd11ce113de4f65534
srbe15211@hotmail.com.tw <srbe15211@gmail.com>
2016-07-31 17:39:40 +0800

    solveThreadSleepinLock

--- Code_Example/MyThreadExample.cs
+++ Code_Example/MyThreadExample.cs
@@ -9,43 +9,50 @@ namespace Code_Example
     {
         private static int count1 = 0;
         private static int count2 = 0;
-        Thread t1;
+        private readonly Thread t1; //readonly : 在run-time initial
         Thread t2;
+        private readonly object m_SyncObj = new object(); //never lock (this) !
+
         public MyThreadExample() {
-            t1 = new Thread(new ThreadStart(increment));
-            t2 = new Thread(new ThreadStart(checkequal));
+            t1 = new Thread(increment) { IsBackground = true }; //這樣寫就可以了
+            t2 = new Thread(new ThreadStart(checkequal)) { IsBackground = true };//isbackground預設false
+            //為什麼要加上isbackground? main結束了 那background thread就會自動跟著結束
+            //如果不設成background thread 那main都結束了 程式卻不會結束 繼續跑thread
         }
         public static void Main() {
             MyThreadExample mt = new MyThreadExample();
             if (count1 == count2) { Console.WriteLine("Thread start"); }
             mt.t1.Start();
             mt.t2.Start();
-            //Console.ReadLine();
+            Console.ReadLine(); //如果不加上readline 當main執行完 程式就結束了(因為thread isbackground=true)
         }
         void increment()
         {
-            lock (this)
+            while (true)
             {
-                while (true)
+                lock (m_SyncObj)
                 {
                     count1++; count2++;
-                    //Thread.Sleep(0);
                 }
+                Thread.Sleep(1000);
             }
         }
         void checkequal()
         {
-            lock (this)
-            {
                 while (true)
                 {
+                    lock (m_SyncObj)
+                    {
+                        Console.WriteLine((count1 == count2) ? "Synchronize" : "unSynchronize");
+                    /*
                     if (count1 == count2)
                         Console.WriteLine("Synchronize");
                     else
                         Console.WriteLine("unSynchronize");
-                   // Thread.Sleep(0);
+                    */
+                    }
+                    Thread.Sleep(1000); 
                 }
-            }
         }
     }
 }
